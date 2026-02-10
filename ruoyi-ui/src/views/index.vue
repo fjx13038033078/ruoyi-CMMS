6ea1eab4eb5cd5cb85cbd147850e9f9eb5a83ffd
范佳兴 <c1111111c@163.com>
2026-02-10 10:22:09 +0800

    feat:修改系统名称

--- ruoyi-ui/src/views/index.vue
+++ ruoyi-ui/src/views/index.vue
@@ -159,7 +159,7 @@ export default {
               left: 'center',
               top: 'center',
               style: {
-                text: '大学生学籍管理平台',
+                text: '校园跑腿任务匹配与管理系统',
                 fontSize: 80,
                 fontWeight: 'bold',
                 lineDash: [0, 200],
